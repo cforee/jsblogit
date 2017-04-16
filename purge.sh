3e1866d48082b7d0ed39c5d0b831b1e3fc24d0ed
cforee <cforee@gmail.com>
2017-04-16 02:19:04 -0700

    update purge/publish scripts - derive source bucket from env variable

--- purge.sh
+++ purge.sh
@@ -1,3 +1,3 @@
-s3cmd del -r s3://tgiaw-production/articles
+s3cmd del -r s3://$JSBLOGIT_SOURCE_BUCKET/articles
 echo '' > ./manifest.html
-s3cmd put ./manifest.html s3://tgiaw-production --acl-public
+s3cmd put ./manifest.html s3://$JSBLOGIT_SOURCE_BUCKET --acl-public
