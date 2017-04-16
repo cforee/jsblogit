900a0fdc279454d3872db539d017390fcef2d565
cforee <cforee@gmail.com>
2017-04-16 02:47:58 -0700

    update ui - slightly smaller reading area width.  Publish manifest to app subfolder

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 s3cmd put -r ./articles/*.md s3://$JSBLOGIT_SOURCE_BUCKET/articles/ --acl-public
 ls -m ./articles > manifest.html
-s3cmd put ./manifest.html s3://$JSBLOGIT_SOURCE_BUCKET --acl-public
+s3cmd put ./manifest.html s3://$JSBLOGIT_SOURCE_BUCKET/jsblogit/ --acl-public
